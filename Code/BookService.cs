2e8455c3560425cb32e86a5bfdf68a1614ae466a
jordan-conway-0 <D00244954@student.dkit.ie>
2024-10-13 16:13:00 +0100

    Add BookService

---
+++ Code/BookService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using RestSharp;
+
+namespace CA2.Code
+{
+	public class BookService
+	{
+		static readonly string URL = "https://www.googleapis.com/books/v1/volumes?";
+		static RestClient client = new RestClient(URL);
+
+
+
+		public BookService()
+		{
+			
+		}
+
+		public RestResponse? getBooks(string tag)
+		{
+			var request = new RestRequest();
+			request.AddParameter("q", tag);
+			var response = client.Execute(request);
+
+			return response;
+		}
+	}
+}
