266145324d21c55cb0aaf8023830831b74a93d37
jordan-conway-0 <D00244954@student.dkit.ie>
2024-10-15 21:29:04 +0100

    Add Book Class

---
+++ Code/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace CA2.Code
+{
+	public class Book
+	{
+		private string _title;
+		private string _subtitle;
+		private string[] _authors;
+		private bool _isMature;
+		private string _image;
+		private string _link;
+
+		public Book(string title)
+		{
+			_title = title;
+		}
+
+		public string Title { get; set; }
+		public string Subtitle { get; set; }
+		public string[] Authors { get; set; }
+		public bool IsMature { get; set; }
+		public string Image { get; set; }
+		public string Link { get; set; }
+
+	}
+}
