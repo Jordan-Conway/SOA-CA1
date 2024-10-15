4f0e220d64b081e89f00d87299df33594003db3a
jordan-conway-0 <D00244954@student.dkit.ie>
2024-10-15 21:31:47 +0100

    Add Video Class

---
+++ Code/Video.cs
@@ -0,0 +1,18 @@
+﻿namespace CA2.Code
+{
+	public class Video
+	{
+		private string _title;
+		private string _description;
+		private string[] _tags;
+
+		public Video(string title)
+		{
+			_title = title;
+		}
+
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string[] Tags { get; set; }
+	}
+}
