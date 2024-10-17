acf5b2213fdf6068bf07c32c68c0c04fcd5addb2
jordan-conway-0 <D00244954@student.dkit.ie>
2024-10-17 18:04:18 +0100

    Add option to export books to file

--- Code/BookService.cs
+++ Code/BookService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 
 namespace CA2.Code
@@ -22,6 +23,67 @@ namespace CA2.Code
 			var response = Client.Execute(request);
 
 			return response;
+		}
+
+		public Book[] parseRespone(JObject jsonObject)
+		{
+			var books = new List<Book>();
+
+			try
+			{
+				var items = jsonObject["items"];
+				if (items == null) {
+					return books.ToArray<Book>();
+				}
+				foreach(var item in items)
+				{
+					JToken? title = item["volumeInfo"]["title"];
+					JToken? subtitle = item["volumeInfo"]["subtitle"];
+					JToken? authors = item["volumeInfo"]["authors"];
+					JToken? isMature = item["volumeInfo"]["maturityRating"];
+					JToken? image = item["volumeInfo"]["imageLinks"]["thumbnail"];
+					JToken? link = item["volumeInfo"]["infoLink"];
+
+					Book newBook;
+					if (title != null)
+					{
+						newBook = new Book(title.ToObject<string>());
+					}
+					else
+					{
+						//A book must have a title. If none is provided, we skip it
+						continue;
+					}
+
+					newBook.Subtitle = subtitle == null ? "" : subtitle.ToObject<string>();
+					newBook.Authors = authors == null ? new List<String>().ToArray() : authors.ToObject<string[]>();
+					
+					if(isMature == null)
+					{
+						newBook.IsMature = false;
+					}
+					else if(isMature.ToObject<string>().Equals("NOT_MATURE"))
+					{
+						newBook.IsMature = false;
+					}
+					else
+					{
+						newBook.IsMature = true;
+					}
+
+					newBook.Image = image == null ? "" : image.ToObject<string>();
+					newBook.Link = link == null ? "" : link.ToObject<string>();
+
+					books.Add(newBook);
+				}
+			}
+			catch(NullReferenceException e)
+			{
+				Console.WriteLine("Encountered exception while parsing books");
+				Console.WriteLine(e.Message);
+			}
+
+			return books.ToArray<Book>();
 		}
 	}
 }
