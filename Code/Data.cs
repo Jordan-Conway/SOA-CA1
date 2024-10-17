335123af51dd0e08ebc02823c7540c26d2e0dbe6
jordan-conway-0 <D00244954@student.dkit.ie>
2024-10-17 16:23:28 +0100

    Fix videoTags becoming null when selecting a tag

---
+++ Code/Data.cs
@@ -0,0 +1,17 @@
+﻿namespace CA2.Code
+{
+    public class Data
+    {
+        private string? _selectedTag;
+        private string[]? _tags;
+
+        public string? SelectedTag { get; set; }
+        public string[]? Tags { get; set; }
+
+        public Data(string? selectedTag, string[]? tags)
+        {
+            SelectedTag = selectedTag;
+            Tags = tags;
+        }
+    }
+}
